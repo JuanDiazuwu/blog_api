f88e2eecb50fa47a2ae9db49afab932be719ccc1
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2024-03-26 21:20:42 -0600

    adding put & delete to comments

--- server/routes/comments.py
+++ server/routes/comments.py
@@ -23,3 +23,17 @@ def find_all_comment():
 @comment.get('/comments/{id}', response_model=Comment, tags=['comments'])
 def find_comment(id:str):
     return commentEntity(connection.my_blog_db.comments.find_one({"_id":ObjectId(id)}))
+
+@comment.put('/comments/{id}', response_model=Comment, tags=['comments'])
+async def update_user(id:str, comment:Comment):
+    connection.my_blog_db.comments.find_one_and_update(
+        {"_id": ObjectId(id)},
+        {"$set": comment.model_dump()},
+        return_document=ReturnDocument.AFTER
+    )
+    return(find_comment(id))
+
+@comment.delete('/comments/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['comments'])
+def comment_delete(id:str):
+    commentEntity(connection.my_blog_db.comments.find_one_and_delete({"_id":ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)
