aa575900096add335be5fe865914110264fa4c6d
CrisRLoera <rcristianrubi@gmail.com>
2024-05-09 12:05:54 -0600

    Buttons for add tags and categories

--- client/src/pages/Publicate.jsx
+++ client/src/pages/Publicate.jsx
@@ -13,18 +13,21 @@ function Publicate() {
     // Variables para almacenar los tags y categorias en la BD
     const [tagsList, setTagsList] = useState({});
     const [categoriesList, setCategoriesList] = useState({});
-    // Variables 
-    const [user, setUser] = useState("");
+    // Variables para agregar publicaciones
+    const [user_id, setUser] = useState("");
     const [content, setContent] = useState ("");
     const [title, setTitle] = useState ("");
     const [tags, setTags] = useState([]);
     const [categories, setCategories] = useState([]);
 
+    const [newTag, setNewTag] = useState("");
+    const [newCategory, setNewCategory] = useState("");
+
     useEffect(() => {
+        setUser(location.state.user_id)
         if (!location.state || !location.state.user_id) {
             setErrorMessage("Usuario no encontrado.");
         }
-        setUser(location.state.user_id)
         const fetchTagsAndCategories = async (e) => {
             try {
                 const resTags = await axios.get('http://localhost:8000/tags');
@@ -41,8 +44,9 @@ function Publicate() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
+            console.log(user_id)
             const res = await axios.post('http://localhost:8000/publications', {
-            user,
+            user_id,
             title,
             content,
             categories,
@@ -50,7 +54,11 @@ function Publicate() {
             });
             if (res.status === 200) {
                 console.log("Respuesta válida")
-                resetForm()
+                setTitle("");
+                setContent("");
+                setTags([]);
+                setCategories([]);
+                setErrorMessage("");
             } else {
                 console.log("Respuesta inválida")
                 setErrorMessage("Formulario incompleto.");
@@ -72,11 +80,63 @@ function Publicate() {
         }
 
     }
-    const resetForm = () => {
-        setTitle('');
-        setContent('');
-        setCategoriesList([]);
-        setTagsList([]);
+
+    const handleAddTags = async () => {
+        
+        try {
+            const res = await axios.post('http://localhost:8000/tags', {
+            "tag_name":newTag
+            });
+            if (res.status === 200) {
+                console.log("Respuesta válida")
+            } else {
+                console.log("Respuesta inválida")
+                setErrorMessage("Formulario incompleto.");
+            }
+        } catch (error) {
+            if (error.response) {
+                // La solicitud fue realizada, pero el servidor respondió con un código de error
+                console.log("Error de respuesta del servidor:", error.response.status);
+                setErrorMessage(`Error: ${error.response.status} ${error.response.statusText}`);
+            } else if (error.request) {
+                // La solicitud se realizó, pero no se recibió respuesta del servidor
+                console.log("No se recibió respuesta del servidor:", error.request);
+                setErrorMessage("No se recibió respuesta del servidor");
+            } else {
+                // Ocurrió un error al configurar la solicitud
+                console.log("Error al configurar la solicitud:", error.message);
+                setErrorMessage(`Error al configurar la solicitud: ${error.message}`);
+            }
+        }
+    };
+
+    const handleAddCategories = async () => {
+        
+        try {
+            const res = await axios.post('http://localhost:8000/categories', {
+            "category_name":newCategory
+            });
+            if (res.status === 200) {
+                console.log("Respuesta válida")
+            } else {
+                console.log("Respuesta inválida")
+                setErrorMessage("Formulario incompleto.");
+            }
+        } catch (error) {
+            if (error.response) {
+                // La solicitud fue realizada, pero el servidor respondió con un código de error
+                console.log("Error de respuesta del servidor:", error.response.status);
+                setErrorMessage(`Error: ${error.response.status} ${error.response.statusText}`);
+            } else if (error.request) {
+                // La solicitud se realizó, pero no se recibió respuesta del servidor
+                console.log("No se recibió respuesta del servidor:", error.request);
+                setErrorMessage("No se recibió respuesta del servidor");
+            } else {
+                // Ocurrió un error al configurar la solicitud
+                console.log("Error al configurar la solicitud:", error.message);
+                setErrorMessage(`Error al configurar la solicitud: ${error.message}`);
+            }
+        }
     };
 
     const handleTagChange = (selectedOptions) => {
@@ -89,7 +149,8 @@ function Publicate() {
 
     return (
         <>
-            <form className='p-10 pb-20 bg-teal-700 rounded-xl block mx-[10%] text-white font-bold' onSubmit={handleSubmit}>
+        <div className='p-10 pb-20 bg-teal-700 rounded-xl mx-[10%] text-white font-bold'>
+            <form className=' block' onSubmit={handleSubmit}>
                 <p>Titulo</p>
                 <input
                     className='rounded text-black font-normal'
@@ -121,6 +182,15 @@ function Publicate() {
                     <button className='bg-emerald-800 block text-white mt-2 p-1 px-3 rounded-2xl mb-10'>Publicar</button>
                 </div>
             </form>
+            <div className='block pt-5'>
+                <input type='text' className='rounded text-black' onChange={(e)=>{setNewTag(e.target.value)}}></input>
+                <button className='bg-emerald-800 rounded-2xl ml-5 p-1 px-3'  onClick={handleAddTags}>Agregar tag</button>
+            </div>
+            <div className='block pt-5'>
+                <input type='text' className='rounded text-black' onChange={(e)=>{setNewCategory(e.target.value)}}></input>
+                <button className='bg-emerald-800 rounded-2xl ml-5 p-1 px-3' onClick={handleAddCategories}>Agregar categoria</button>
+            </div>
+        </div>
         </>
     )
 }
